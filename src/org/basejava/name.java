0c29a5cc851a4d0fbdb6239f422253e22a33e43e
java <deepan2104d@gmail.com>
2021-10-23 21:28:58 +0530

    local

---
+++ src/org/basejava/name.java
@@ -0,0 +1,27 @@
+package org.basejava;
+
+public class name {
+	public static void main(String[] args) {
+		byte age =21;
+		System.out.println(age);
+		short atmpin=7668;
+		System.out.println("my atm pin is:"+atmpin);
+		int lanline=12345676;
+		System.out.println(lanline);
+		long mobile=8878786767l;
+		System.out.println(mobile);
+		float salary=4242.6567564646f;
+		System.out.println(salary);
+		char d = 'S';
+		System.out.println(d);
+		String name ="deepan";
+		boolean id=true;
+		System.out.println(id);
+		float ds=20/3f;
+		System.out.println(ds);
+		double dsl=20/3d;
+		System.out.println(dsl);
+		
+	}		
+		
+}
